d23a67be37ced7d44edef3700a6c273160007e07
Matt Shim (TEKsystems) <v-shimma@microsoft.com>
2017-11-07 14:14:14 -0800

    initial commit

---
+++ Luis-Scorable-Qna/Dialogs/CommonResponsesDialog.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Scorable.Dialogs
+{
+
+
+    public class CommonResponsesDialog : IDialog<object>
+    {
+        private readonly string _messageToSend;
+        private Activity _activity;
+
+        public CommonResponsesDialog(string message)
+        {
+            _messageToSend = message;
+        }
+
+        // overload constructor to handle activities (card attachments)
+        public CommonResponsesDialog(Activity activity)
+        {
+            var heroCard = new HeroCard
+            {
+                Title = "Help",
+                Text = "Some Text Here",
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Get Started", value: "https://docs.microsoft.com/bot-framework") }
+            };
+            var reply = activity.CreateReply();
+            var attachments = new List<Attachment>();
+            attachments.Add(heroCard.ToAttachment());
+            reply.Attachments = attachments;
+            _activity = reply;
+
+        }
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            if (string.IsNullOrEmpty(_messageToSend))
+            {
+                await context.PostAsync(_activity);
+            }
+            else
+            {
+                await context.PostAsync(_messageToSend);
+            }
+            context.Done<object>(null);
+        }
+    }
+}
---
+++ Luis-Scorable-Qna/Dialogs/LuisDialog.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
+using System.Threading;
+
+namespace Scorable.Dialogs
+{
+    [Serializable]
+    [LuisModel("e7a9c2d5-0b92-47d3-9d73-d35fb45c1b8e", "4941fa348c49494db1e8e8d2fd7adb2c")]
+    public class LuisDialog : LuisDialog<object>
+    {
+        private object dummyactivity;
+
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = $"Sorry, I did not understand '{result.Query}'. Type 'help' if you need assistance.";
+
+            await context.PostAsync(message);
+
+            context.Wait(this.MessageReceived);
+        }
+
+        [LuisIntent("greeting")]
+        public async Task Greeting(IDialogContext context, LuisResult result)
+        {
+            string message = $"Hello there";
+
+            await context.PostAsync(message);
+
+            context.Wait(this.MessageReceived);
+        }
+
+        private ResumeAfter<object> after()
+        {
+            return null;
+        }
+
+        [LuisIntent("weather")]
+        public async Task Middle(IDialogContext context, LuisResult result)
+        {
+            // confirm we hit weather intent
+            string message = $"Weather forecast is...";
+
+            await context.PostAsync(message);
+
+            context.Wait(this.MessageReceived);
+        }
+
+        [LuisIntent("joke")]
+        public async Task Joke(IDialogContext context, LuisResult result)
+        {
+            // confirm we hit joke intent
+            string message = $"Let's see...I know a good joke...";
+
+            await context.PostAsync(message);
+
+            await context.Forward(new JokeDialog(), ResumeAfterJokeDialog, dummyactivity, CancellationToken.None);
+        }
+
+        [LuisIntent("question")]
+        public async Task QnA(IDialogContext context, LuisResult result)
+        {
+            // confirm we hit QnA
+            string message = $"Routing to QnA... ";
+
+            var userQuestion = (context.Activity as Activity).Text;
+            await context.Forward(new QnaDialog(), ResumeAfterQnA, context.Activity, CancellationToken.None);
+            await context.PostAsync(message);
+
+            //context.Wait(this.MessageReceived);
+            // await context.Forward(new JokeDialog(), ResumeAfterJokeDialog, activity, CancellationToken.None);
+            //await Conversation.SendAsync(activity, () => new QnaDialog());
+            
+        }
+
+        private async Task ResumeAfterQnA(IDialogContext context, IAwaitable<object> result)
+        {
+            context.Done<object>(null);
+        }
+
+        private async Task ResumeAfterJokeDialog(IDialogContext context, IAwaitable<object> result)
+        {
+            context.Done<object>(null);
+        }
+
+    }
+}
